c0ff7de102498b04b66a8c33714da249d336c42f
Steve Van Loon <steve@pointdrive.com>
2015-02-14 10:04:40 -0600

    Updated cachematrix.R to implement the cacheSolve and makeCacheMatrix functions.  This is part of the R Programming class offered on Coursera around Feb 2015.

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Provide a caching mechanism for a matrix's inverse
+## to cut down computation costs
 
-## Write a short comment describing this function
-## this is a test
+## makeCacheMatrix decorates a matrix by adding a inverse property
+## get - returns the original matrix
+## set - updates the matrix and invalidates the inverse
+## setmean - set the value of the inverse property
+## getmean - get the value of the inverse property
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is a utility method to retrieve the
+## inverse property from the wrapped matrix.
+## If the inverse property is not set, it will
+## solve the inverse and set the property.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
